e19cd95377c4ee936f718982cd6764a719a6a992
timmetje27 <timvanbakel09@gmail.com>
2026-01-13 15:15:22 +0100

    level selector toegevoegd

---
+++ level_selector.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class level_selector here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class level_selector extends World
+{
+
+    /**
+     * Constructor for objects of class level_selector.
+     * 
+     */
+    public level_selector()
+    {    
+        // Create a new world with 1536x1024 cells with a cell size of 1x1 pixels.
+        super(1536, 1024, 1); 
+    }
+}
