4af2d6ce357b2504625f367f6c77fcddd1f3e763
hordijkjasper6-byte <hordijkjasper6@gmail.com>
2026-01-14 20:19:06 +0100

    begin

--- hoofdpersoon.java
+++ hoofdpersoon.java
@@ -1,4 +1,5 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
+import java.lang.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class hoofdpersoon here.
@@ -6,14 +7,46 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class hoofdpersoon extends Actor
-{
+{  
     /**
      * Act - do whatever the hoofdpersoon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public String animationlengt;
     public void act()
     {
-        // Add your action code here.
+        if (greenfoot.isKeyDown("w","a","s","d")){
+            movesprite();
+        }
+        
+        
+    }
+
+    public void setsprite(){
+        
+
+        
+    }
+    public void movesprite(){
+        if (greenfoot.isKeyDown("w")){
+            setLocation(getX(), getY()-1);
+            setsprite();
+        }
+        if (greenfoot.isKeyDown("a")){
+            setLocation(getX()-1, getY());
+            setsprite();
+        }
+        if (greenfoot.isKeyDown("d")){
+            if (greenfoot.isKeyDown("shift")){
+                setLocation(getX()+3, getY());
+                setsprite(2);
+            } else {
+                setLocation(getX()+1, getY());
+                setsprite(1);
+
+            }
+        }
     }
 }
