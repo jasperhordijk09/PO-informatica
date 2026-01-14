53228ee46bfc4752907e84792a85fe8991a1d0e1
timmetje27 <timvanbakel09@gmail.com>
2026-01-14 10:59:37 +0100

    park achtegrond in beweging

---
+++ achtergrond_park.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class achtergrond_park here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class achtergrond_park extends Actor
+{
+    /**
+     * Act - do whatever the achtergrond_park wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(-1);
+        if(getX() == 1024 / 8 * 3 ){
+            setLocation(1024, getY());
+        }
+    }
+}
