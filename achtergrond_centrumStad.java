d241186496cbb8ae91207b9233ae3ad88ff3a4b4
timmetje27 <timvanbakel09@gmail.com>
2026-01-29 14:29:35 +0100

    level selector

--- achtergrond_centrumStad.java
+++ achtergrond_centrumStad.java
@@ -3,21 +3,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class achtergrond_centrumStad here.
  * 
- * @author (your name) 
+3* @author (your name) 
  * @version (a version number or a date)
  */
 public class achtergrond_centrumStad extends Actor
 {
-    /**
-     * Act - do whatever the achtergrond_centrumStad wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
-        move(-1);
-        if(getX() == 1024 / 8 * 3 ){
-            setLocation(1024, getY());
-        }
-    }
+	/**
+	 * Act - do whatever the achtergrond_centrumStad wants to do. This method is called whenever
+	 * the 'Act' or 'Run' button gets pressed in the environment.
+	 */
+	public void act()
+	{
+		// Add your action code here.
+		move(-1);
+		if(getX() == 1024 / 8 * 3 ){
+			setLocation(1024, getY());
+		}
+	}
 }
+					
---
+++ level_centrum.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class level_centrum here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class level_centrum extends levels_selector
+{
+    /**
+     * Act - do whatever the level_centrum wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        
+    }
+}
