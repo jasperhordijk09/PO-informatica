702da7913cc7bb5e19d7e5b56bb8b321e38f9961
Floris789 <florisprive1@gmail.com>
2026-02-03 09:41:14 +0100

    icons in de wereld gezet

--- level_selector.java
+++ level_selector.java
@@ -17,5 +17,12 @@ public class level_selector extends World
     {    
         // Create a new world with 1536x1024 cells with a cell size of 1x1 pixels.
         super(1536, 1024, 1); 
+        addObject(new level_centrum(), 820, 310);
+        addObject(new level_fabriek(), 1250, 560);
+        addObject(new level_industrie(), 1100, 815);
+        addObject(new level_pakhuis(), 1100, 160);
+        addObject(new level_park(), 375, 535);
+        addObject(new level_werkplaats(), 137, 200);
+
     }
 }
