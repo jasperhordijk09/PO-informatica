36be46af81159e477890fcf09801d0ecebe85184
hordijkjasper6-byte <hordijkjasper6@gmail.com>
2026-02-12 10:57:05 +0100

    changes

--- Block.java
+++ Block.java
@@ -5,6 +5,5 @@ public class Block extends Actor {
     public Block(String imageName) {
         setImage(imageName);
     }
-
-    // Default friction (normal ground)
 }
+
--- hoofdpersoon.java
+++ hoofdpersoon.java
@@ -1,30 +1,29 @@
-  import greenfoot.*;
+import greenfoot.*;
 
 public class Hoofdpersoon extends Personages {
 
-    public static boolean debugHitbox = false;
-
     private AnimationManager manager;
     private AnimationAnimator animator;
-
     private boolean facingLeft = false;
 
-    private double vSpeed = 0;
-    private double gravity = 0.5;
-    private int jumpStrength = -10;
-
-    private double hSpeed = 0;
-    private double moveSpeed = 0.7;
-
-    private int maxStepHeight = 3;
-
     private String[] WALK_ORDER = {
-        "sprite_002.png","sprite_003.png","sprite_004.png","sprite_005.png",
-        "sprite_006.png","sprite_007.png","sprite_008.png","sprite_009.png",
-        "sprite_010.png","sprite_011.png","sprite_012.png","sprite_013.png"
+        "sprite_002.png",
+        "sprite_003.png",
+        "sprite_004.png",
+        "sprite_005.png",
+        "sprite_006.png",
+        "sprite_007.png",
+        "sprite_008.png",
+        "sprite_009.png",
+        "sprite_010.png",
+        "sprite_011.png",
+        "sprite_012.png",
+        "sprite_013.png",
     };
 
-    private String[] IDLE_ORDER = { "sprite_001.png" };
+    private String[] IDLE_ORDER = {
+        "sprite_001.png"
+    };
 
     public Hoofdpersoon() {
         manager = new AnimationManager();
@@ -42,109 +41,43 @@ public class Hoofdpersoon extends Personages {
     }
 
     public void act() {
-        toggleDebug();
-        handleJump();
         handleMovement();
-        applyGravity();
-        applyFriction();
-        limitspeed();
+        handleJumping();
+        handleGravity();
         setImage(animator.update());
-        xhoofdpersoon = getX();
-        yhoofdpersoon = getY();
-        System.out.println(yhoofdpersoon);
-    }
-
-    private void toggleDebug() {
-        if (Greenfoot.isKeyDown("f3")) {
-            debugHitbox = !debugHitbox;
-            Greenfoot.delay(10);
-        }
-    }
-
-    private int getInputDirection() {
-        if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) return -1;
-        if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) return 1;
-        return 0;
-    }
-
-    private boolean isAbove(Block block) {
-        int playerBottom = getY() + getImage().getHeight() / 2;
-        int blockTop = block.getY() - block.getImage().getHeight() / 2;
-        return playerBottom <= blockTop;
-    }
-
-    public void limitspeed() {
-        Block block = (Block)getOneObjectAtOffset(0, getImage().getHeight()/2, Block.class);
-        if (block instanceof SlimeBlock){
-            if (hSpeed > 2.9) hSpeed = 3;
-            if (hSpeed < -2.9) hSpeed = -3;
-        } else {
-            if (hSpeed < -4.9) hSpeed = -5;
-            if (hSpeed > 4.9) hSpeed = 5;
-        }
     }
 
     private void handleMovement() {
         if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) {
             facingLeft = true;
-            hSpeed -= moveSpeed;
+            setLocation(getX() - 3, getY());
             animator.play("WalkingLeft");
         }
         else if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) {
             facingLeft = false;
-            hSpeed += moveSpeed;
+            setLocation(getX() + 3, getY());
             animator.play("WalkingRight");
         }
         else {
             animator.play(facingLeft ? "IdleLeft" : "IdleRight");
         }
-
-        setLocation(getX() + (int)hSpeed, getY());
     }
 
-    private void handleJump() {
-        if (Greenfoot.isKeyDown("space") && onGround()) {
-            vSpeed = jumpStrength;
+    private void handleJumping() {
+        if(onGround() && (Greenfoot.isKeyDown("space"))) {
+            setLocation(getX(), getY() - 10); // Simulate a jump by moving the character up
         }
     }
 
-    private void applyGravity() {
-        vSpeed += gravity;
-        setLocation(getX(), (int)(getY() + vSpeed));
-
-        Block blockBelow = (Block)getOneObjectAtOffset(0, getImage().getHeight()/2, Block.class);
-
-        if (blockBelow != null && isAbove(blockBelow) && vSpeed >= 0) {
-            int blockTop = blockBelow.getY() - blockBelow.getImage().getHeight() / 2;
-            setLocation(getX(), blockTop - getImage().getHeight() / 2);
-            vSpeed = 0;
+    private void handleGravity() {
+        if(!onGround()) {  
+            setLocation(getX(), getY() + 5); // Simulate gravity by moving the character down
         }
     }
-
-    private void applyFriction() {
-        Block block = (Block)getOneObjectAtOffset(0, getImage().getHeight()/2, Block.class);
-
-        double friction = 0.5;
-        if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) {
-            if(block instanceof SlimeBlock) friction = 1.2;
-            else if(block instanceof IceBlock) friction = 0.8;
-            else friction = 1;
-        }
-        else {
-            if(block instanceof SlimeBlock) friction = 1.4;
-            else if(block instanceof IceBlock) friction = 1.2;
-            else friction = 1.3;
-        }
-
-        hSpeed /= friction;
-
-        if (Math.abs(hSpeed) < 0.1) hSpeed = 0;
-    }
-
+    
     private boolean onGround() {
-        setLocation(getX(), getY() + 1);
-        boolean touching = isTouching(Block.class);
-        setLocation(getX(), getY() - 1);
-        return touching;
+        Block block = (Block) getOneIntersectingObject(Block.class);
+        return block != null;
     }
+
 }
