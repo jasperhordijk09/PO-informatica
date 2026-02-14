123d1839fc785af3a851cd3e5dff75ef2e01e5ba
timmetje27 <timvanbakel09@gmail.com>
2026-02-14 14:31:01 +0100

    achtergronden bewegen met hoofdpersoon

--- achtergrond_industrie.java
+++ achtergrond_industrie.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Write a description of class achtergrond_industrie here.
@@ -6,18 +7,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class achtergrond_industrie extends Actor
+public class achtergrond_industrie extends Achtergronden
 {
+    private int lastPlayerX = Integer.MIN_VALUE;
     /**
      * Act - do whatever the achtergrond_industrie wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
-        move(-1);
-        if(getX() == 1024 / 8 * 3 ){
-            setLocation(1024, getY());
-    }
+        if (getWorld() == null) return;
+
+        List<Hoofdpersoon> players = getWorld().getObjects(Hoofdpersoon.class);
+        if (players.isEmpty()) return;
+
+        Hoofdpersoon p = players.get(0);
+        int px = p.getX();
+
+        if (lastPlayerX == Integer.MIN_VALUE) {
+            lastPlayerX = px;
+            return;
+        }
+
+        int dx = px - lastPlayerX;
+        setLocation(getX() - dx, getY());
+
+        lastPlayerX = px;
 }
 }
